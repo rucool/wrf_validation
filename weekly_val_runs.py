915ddb0a5dbbcb1fdadeded169c3d68e3bcbbb49
Lori Garzio <lgarzio@marine.rutgers.edu>
2022-07-24 12:15:59 -0400

    Update weekly_val_runs.py

--- weekly_val_runs.py
+++ weekly_val_runs.py
@@ -6,8 +6,6 @@ import argparse
 import sys
 import numpy as np
 import pandas as pd
-import matplotlib
-matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from datetime import datetime, timedelta
