aec9cf6315649c59aef479fe469bc52c8bb3d37c
Lori Garzio <lgarzio@marine.rutgers.edu>
2022-08-24 10:29:24 -0400

    Create check_data_flow_cmoms.py

---
+++ check_data_flow/check_data_flow_cmoms.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""
+Author: Lori Garzio on 8/24/2022
+Last modified: Lori Garzio 8/24/2022
+Check data flow for the Coastal Metocean Monitoring Station.
+"""
+
+import os
+import sys
+import numpy as np
+import pandas as pd
+import glob
+
+
+def main():
+    met_dir = '/home/coolgroup/MetData/CMOMS/surface_temp/surface'
+
+    # find the most recent file
+    last_file = sorted(glob.glob(os.path.join(met_dir, '*.csv')))[-1]
+    df = pd.read_csv(last_file)
+
+    tm = pd.to_datetime(np.array(df['time_stamp(utc)']))
+    last_ts = pd.to_datetime(np.nanmax(tm)).strftime('%Y-%m-%d %H:%M')
+
+    # return the most recent timestamp
+    print(last_ts)
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
