94bf7b026875917883c65efdbea168df8877004c
Lori Garzio <lgarzio@marine.rutgers.edu>
2022-09-29 13:02:37 -0400

    Update check_data_flow_atlanticshores.py
    add argument for height

--- check_data_flow/check_data_flow_atlanticshores.py
+++ check_data_flow/check_data_flow_atlanticshores.py
@@ -2,7 +2,7 @@
 
 """
 Author: Lori Garzio on 8/24/2022
-Last modified: Lori Garzio 8/24/2022
+Last modified: Lori Garzio 8/29/2022
 Check latest timestamp for the wind dataset from Atlantic Shores buoy data:
 https://erddap.maracoos.org/erddap/search/index.html?page=1&itemsPerPage=1000&searchFor=atlantic+shores
 """
@@ -16,6 +16,7 @@ from erddapy import ERDDAP
 
 def main(args):
     buoy = args.buoy
+    height = args.height
 
     url_erddap = 'https://erddap.maracoos.org/erddap/'
     dataset_id = f'AtlanticShores_{buoy}_wind'
@@ -32,13 +33,8 @@ def main(args):
     ]
 
     constraints = {
-        'altitude=': 10
+        'altitude=': height
     }
-
-    e = ERDDAP(server=url_erddap,
-               protocol='tabledap',
-               response='nc'
-               )
 
     e.dataset_id = dataset_id
     e.constraints = constraints
@@ -46,10 +42,13 @@ def main(args):
 
     ds = e.to_xarray()
 
-    # find the latest timestamp
-    last_ts = pd.to_datetime(np.nanmax(ds.time.values)).strftime('%Y-%m-%d %H:%M')
+    # find the latest timestamp with valid data (it appears that their fill value is 59.99255)
+    idx = np.where(np.logical_and(ds.wind_speed >= 0, ds.wind_speed < 59.9))[0]
+    valid_ts = ds.time.values[idx]
 
-    # return the most recent model run timestamp
+    last_ts = pd.to_datetime(np.nanmax(valid_ts)).strftime('%Y-%m-%d %H:%M')
+
+    # return the most recent timestamp containing valid data for the height of interest
     print(f'{last_ts} GMT')
 
 
@@ -63,6 +62,13 @@ if __name__ == '__main__':
                             type=str,
                             choices=['ASOW-4', 'ASOW-6'],
                             help='Atlantic Shores buoy code')
+
+    arg_parser.add_argument('-z', '--height',
+                            dest='height',
+                            default=80,
+                            type=int,
+                            choices=[10, 80, 160],
+                            help='choose a height in meters')
 
     parsed_args = arg_parser.parse_args()
     sys.exit(main(parsed_args))
